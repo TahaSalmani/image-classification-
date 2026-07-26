7d630470af04458e49da274c9bf39787baed3cb0
Taha <the.taha.salmani@gmail.com>
2026-07-26 23:09:25 +0330

    Evaluation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from cnnclassifier import  logger
 from cnnclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
 from  cnnclassifier.pipeline.stage_02_prepare_base_model import  PreparingBaseModelTrainingPipeline
 from  cnnclassifier.pipeline.stage_03_training import ModelTrainingPipeline
-
+from  cnnclassifier.pipeline.stage_04_Evaluation import EvaluationPipeline
 STAGE_NAME = "Data Ingestion Stage "
 if __name__ == "__main__":
     try:
@@ -45,4 +45,31 @@ if __name__ == "__main__":
         raise e
 
 
+
+STAGE_NAME = "Training"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+    raise e
 
--- oop.py
+++
@@ -1,4 +0,0 @@
-from dataclasses import dataclass
-from pathlib import Path
-import  tensorflow as tf
-model = tf.keras.applications.VGG16.VGG16(include_top=False, weights='imagenet')
