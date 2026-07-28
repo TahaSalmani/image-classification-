38bede714ad5f13b9577071ac28678d9f6f29e42
Taha <the.taha.salmani@gmail.com>
2026-07-28 20:29:53 +0330

    remove unused import

--- src/cnnclassifier/pipeline/predict.py
+++ src/cnnclassifier/pipeline/predict.py
@@ -1,5 +1,4 @@
 import numpy as np
-from anaconda_project.internal.conda_api import result
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
