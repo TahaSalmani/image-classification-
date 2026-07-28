7bd58daa12699043d22addd4fc3a0bc7741c5f44
Taha <the.taha.salmani@gmail.com>
2026-07-28 20:21:57 +0330

    Add App.py Changes2

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath("."))
-
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS, cross_origin
 from cnnclassifier.utils.common import decodeimage
