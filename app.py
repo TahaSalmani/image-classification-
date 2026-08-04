14aee0a06e90fe43f38f20000be89b96a3fa564c
Taha <the.taha.salmani@gmail.com>
2026-08-04 12:48:26 +0330

    Add debug logging and auth basic for dvc pull

--- app.py
+++ app.py
@@ -15,9 +15,13 @@ dagshub_user = os.environ.get("DAGSHUB_USERNAME")
 dagshub_token = os.environ.get("DAGSHUB_TOKEN")
 
 if dagshub_user and dagshub_token:
+    print(f"DEBUG: DAGSHUB_USERNAME is set (len={len(dagshub_user)})")
+    print(f"DEBUG: DAGSHUB_TOKEN is set (len={len(dagshub_token)})")
+    os.system('dvc remote modify origin --local auth basic')
     os.system(f'dvc remote modify origin --local user "{dagshub_user}"')
     os.system(f'dvc remote modify origin --local password "{dagshub_token}"')
-    os.system("dvc pull artifacts/training/model.h5 -r origin")
+    exit_code = os.system("dvc pull artifacts/training/model.h5 -r origin -v")
+    print(f"DEBUG: dvc pull exit code = {exit_code}")
 else:
     print("WARNING: DAGSHUB_USERNAME / DAGSHUB_TOKEN not set, skipping dvc pull.")
 
